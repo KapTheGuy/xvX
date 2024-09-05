946f48ec3f40f4f14baec79a2b4659e13d320758
Kap Petrov <pathaksabita31@gmail.com>
2024-09-05 13:03:39 +0530

    xvX 0.3.5

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
+#include "monitor.h"
+#include "descriptor_tables.h"
+#include "timer.h"
+
 int main(struct multiboot *mboot_ptr)
 {
-  // All our initialisation calls will go in here.
-  return 0xDEADBABA;
+	// Initialise all the ISRs and segmentation
+    init_descriptor_tables();
+
+	monitor_clear();
+	monitor_write("xvX Operating System, Kernel 0.3.5\n");
+
+	asm volatile("int $0x3");
+    asm volatile("int $0x4");
+
+    asm volatile("sti");
+
+    init_timer(50);
+
+  	return 0;
 } 
---
+++ timer.h
@@ -0,0 +1,13 @@
+/*
+	PIT Timer
+	Developed by xvX Developers <Adaptable Hybrid Kernel Team>
+*/
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "common.h"
+
+void init_timer(u32int frequency);
+
+#endif
